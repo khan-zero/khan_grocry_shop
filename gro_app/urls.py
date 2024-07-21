c3107127281ffe69303221cbfdb48531e2afffdf
SaidAbbosxon <mr.khan.r35@gmail.com>
2024-07-21 23:49:18 +0500

    grocry

---
+++ gro_app/urls.py
@@ -0,0 +1,40 @@
+from django.urls import path
+from .views import (
+    IndexView, Index2View, Index3View, Index4View, Index5View, Index6View, Index7View, Index8View,
+    AboutView, ServiceView, TeamView, FaqView, LoginView, ShopView, ShopListView, ShopDetailsView,
+    CartView, CheckoutView, WishlistView, BlogView, BlogListView, BlogGridView, BlogGridSidebarView,
+    BlogMasonryView, BlogDetailsView, ErrorView, ContactView
+)
+
+urlpatterns = [
+    path('', IndexView.as_view(), name='index'),
+    path('index/', IndexView.as_view(), name='index'),
+    path('index2/', Index2View.as_view(), name='index2'),
+    path('index3/', Index3View.as_view(), name='index3'),
+    path('index4/', Index4View.as_view(), name='index4'),
+    path('index5/', Index5View.as_view(), name='index5'),
+    path('index6/', Index6View.as_view(), name='index6'),
+    path('index7/', Index7View.as_view(), name='index7'),
+    path('index8/', Index8View.as_view(), name='index8'),
+
+    path('about/', AboutView.as_view(), name='about'),
+    path('service/', ServiceView.as_view(), name='service'),
+    path('team/', TeamView.as_view(), name='team'),
+    path('faq/', FaqView.as_view(), name='faq'),
+    path('login-register/', LoginView.as_view(), name='login-register'),
+    path('shop/', ShopView.as_view(), name='shop'),
+    path('shop-list/', ShopListView.as_view(), name='shop-list'),
+    path('shop-details/', ShopDetailsView.as_view(), name='shop-details'),
+    path('cart/', CartView.as_view(), name='cart'),
+    path('checkout/', CheckoutView.as_view(), name='checkout'),
+    path('wishlist/', WishlistView.as_view(), name='wishlist'),
+    path('blog/', BlogView.as_view(), name='blog'),
+    path('blog-list/', BlogListView.as_view(), name='blog-list'),
+    path('blog-grid/', BlogGridView.as_view(), name='blog-grid'),
+    path('blog-grid-sidebar/', BlogGridSidebarView.as_view(), name='blog-grid-sidebar'),
+    path('blog-masonry/', BlogMasonryView.as_view(), name='blog-masonry'),
+    path('blog-details/', BlogDetailsView.as_view(), name='blog-details'),
+    path('error/', ErrorView.as_view(), name='error'),
+    path('contact/', ContactView.as_view(), name='contact'),
+]
+
---
+++ gro_app/views.py
@@ -0,0 +1,82 @@
+from django.views.generic import TemplateView
+
+class IndexView(TemplateView):
+    template_name = 'index.html'
+
+class Index2View(TemplateView):
+    template_name = 'index-2.html'
+
+class Index3View(TemplateView):
+    template_name = 'index-3.html'
+
+class Index4View(TemplateView):
+    template_name = 'index-4.html'
+
+class Index5View(TemplateView):
+    template_name = 'index-5.html'
+
+class Index6View(TemplateView):
+    template_name = 'index-6.html'
+
+class Index7View(TemplateView):
+    template_name = 'index-7.html'
+
+class Index8View(TemplateView):
+    template_name = 'index-8.html'
+
+class AboutView(TemplateView):
+    template_name = 'about.html'
+
+class ServiceView(TemplateView):
+    template_name = 'service.html'
+
+class TeamView(TemplateView):
+    template_name = 'team.html'
+
+class FaqView(TemplateView):
+    template_name = 'faq.html'
+
+class LoginView(TemplateView):
+    template_name = 'login-register.html'
+
+class ShopView(TemplateView):
+    template_name = 'shop.html'
+
+class ShopListView(TemplateView):
+    template_name = 'shop-list.html'
+
+class ShopDetailsView(TemplateView):
+    template_name = 'shop-details.html'
+
+class CartView(TemplateView):
+    template_name = 'cart.html'
+
+class CheckoutView(TemplateView):
+    template_name = 'checkout.html'
+
+class WishlistView(TemplateView):
+    template_name = 'wishlist.html'
+
+class BlogView(TemplateView):
+    template_name = 'blog.html'
+
+class BlogListView(TemplateView):
+    template_name = 'blog-list.html'
+
+class BlogGridView(TemplateView):
+    template_name = 'blog-grid.html'
+
+class BlogGridSidebarView(TemplateView):
+    template_name = 'blog-grid-sidebar.html'
+
+class BlogMasonryView(TemplateView):
+    template_name = 'blog-masonry.html'
+
+class BlogDetailsView(TemplateView):
+    template_name = 'blog-details.html'
+
+class ErrorView(TemplateView):
+    template_name = 'error.html'
+
+class ContactView(TemplateView):
+    template_name = 'contact.html'
